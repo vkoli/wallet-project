21eda4a756b7e74462a92fc1215b3210c4c6ee0c
Aaron Basch <aaronbasch@aarons-air.resource.campus.njit.edu>
2021-11-22 14:42:44 -0500

    added function names to queries file

--- queries.py
+++ queries.py
@@ -9,3 +9,46 @@ def user_sign_in(ssn, phone):
 def user_sign_up(name, ssn, phone, email):
     #TODO replaced with sql query to add user in db
 	print(f'\nUser {name} identified by {ssn} with phone {phone} and email {email} has signed up.\nPlease sign in to access account.\n')
+
+def account_summary():
+    pass
+
+def add_new_email():
+    pass
+
+def remove_email():
+    pass
+
+def add_new_phone():
+    pass
+
+def remove_phone():
+    pass
+
+def add_new_bank_acc():
+    pass
+
+def remove_bank_acc():
+    pass
+
+def send_transaction():
+    pass
+
+def request_transaction():
+    pass
+
+def statement_users_by_date_range():
+    pass
+
+def statement_users_by_month():
+    pass
+
+def max_transactions():
+    pass
+
+def best_users():
+    pass
+
+def statement_search(type='statement'):
+    pass
+
